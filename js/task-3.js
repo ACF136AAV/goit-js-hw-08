3b7999cd711e09c732788829590aa3b2e0660c00
ACF136 <andrusacf@ukr.net>
2024-03-28 17:20:42 +0200

    complete hw-8

--- js/task-3.js
+++
@@ -1,7 +0,0 @@
-const input = document.getElementById('name-input');
-const output = document.getElementById('name-output');
-
-input.addEventListener('input', () => {
-  const inputValue = input.value.trim();
-  output.textContent = inputValue || 'Anonymous';
-});
